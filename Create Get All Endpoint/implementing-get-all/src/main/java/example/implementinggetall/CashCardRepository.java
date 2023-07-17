537601bdcbb90df3db0ed6eff7a254d73d03901c
Ahmed Yusuf <139216046+ayusuf-aerlingus@users.noreply.github.com>
2023-07-17 11:49:37 +0300

    spring-academy: add create get all endpoint exercise.

---
+++ Create Get All Endpoint/implementing-get-all/src/main/java/example/implementinggetall/CashCardRepository.java
@@ -0,0 +1,9 @@
+package example.implementinggetall;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+@Repository
+public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
+}
---
+++ Create Get All Endpoint/implementing-get-all/src/main/java/example/implementinggetall/ImplementingGetAllApplication.java
@@ -0,0 +1,13 @@
+package example.implementinggetall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImplementingGetAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImplementingGetAllApplication.class, args);
+    }
+
+}
